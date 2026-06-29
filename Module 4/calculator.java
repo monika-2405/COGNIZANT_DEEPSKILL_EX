5e6581de756b289f7de74eee211f6830ea1797d2
MONIKA M <monikamohann@gmail.com>
2026-06-29 21:44:21 +0530

    Add files via upload

---
+++ Module 4/calculator.java
@@ -0,0 +1,12 @@
+package aaa;
+
+public class calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
